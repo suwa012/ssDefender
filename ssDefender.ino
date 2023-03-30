ee3885fb7875b2b603236151150c0131b32c50d3
Adesuwa <adesuwa.dawodu@principia.edu>
2023-03-30 14:03:03 -0500

    Finish code for vertical aim, organise actions

--- ssDefender.ino
+++ ssDefender.ino
@@ -1,40 +1,46 @@
 #include <Servo.h>
 
-Servo baseServo;
-Servo linearServo;
 int RELAY_PIN1=6;
 int RELAY_PIN2=5;
-int REGULAR_SERVO_POWER=RELAY_PIN2;
+
+Servo baseServo;
+Servo linearServo;
+int LINEAR_SERVO_POWER = RELAY_PIN1;
+
+int BALL_LOADER_SERVO_POWER=RELAY_PIN2;
 
 void setup() {
     baseServo.attach(10);
     linearServo.attach(7);
+    linearServo.writeMicroseconds(1000);
     pinMode(RELAY_PIN1, OUTPUT);
     pinMode(RELAY_PIN2, OUTPUT);
+    delay(5000);
 }
 
 void loop() {
     int horizontal_pos[9] = {35.78, 36.46, 55.29, 66.05, 90, 113.95, 124.71, 143.54, 144.22};
-    int vertical_pos[3] = {90, 180, 270};
+    int vertical_pos[9] = {0.25, 0, 0.30, 0, 0.35, 0, 0.30, 0, 0.25};
 
     for(int i = 0; i <= 9; i++) {
-        // Turn on linear actuator
-        digitalWrite(RELAY_PIN1, HIGH);
-        
+        // Load nerf ball
+        digitalWrite(BALL_LOADER_SERVO_POWER, LOW);
+        delay(800);
+        digitalWrite(BALL_LOADER_SERVO_POWER, HIGH);
+        delay(500);
+
+        // AIM barrel
+
+        // Supply power to linear actuator
+        digitalWrite(LINEAR_SERVO_POWER, LOW);
         // Horizontal aim (rotary servo)
         baseServo.write(horizontal_pos[i]);
-
         // Vertical aim (linear actuator)
-        linearServo.write(vertical_pos[i%3]);
-        delay(500);
+        linearServo.writeMicroseconds(1000 + (1000 * vertical_pos[i]));
+        delay(3000); // Delay so linear servo can complete route
+        // Cut off power to linear actuator
+        digitalWrite(LINEAR_SERVO_POWER, HIGH);
 
-        // Load nerf ball
-        digitalWrite(REGULAR_SERVO_POWER, HIGH);
-        delay(500);
-        digitalWrite(REGULAR_SERVO_POWER, LOW);
-
-        // Turn off linear actuator
-        // digitalWrite(RELAY_PIN1, HIGH);
-        delay(1000);
+        // SHOOT
     }
 }
