92f5dfd2bae5c5ef87459438a9f40a6e37f4cb5a
Adesuwa <adesuwa.dawodu@principia.edu>
2023-04-20 13:56:11 -0500

    Fix angle errors

--- aim.ino
+++ aim.ino
@@ -17,28 +17,19 @@ void aimBarrel(float horizontal, float vertical) {
     float newHorizontal;
 
     if(horizontal <= 180) {
-        float dist = midpoint - 500; // 60 gets me what should be 90 degrees
-        float distInMicroseconds = (horizontal / 180) * midpoint;
+        float dist = midpoint - 500;
+        float distInMicroseconds = (horizontal / 180) * dist;
         newHorizontal = 500 + distInMicroseconds;
     }
 
     // Supply power to linear actuator
     digitalWrite(LINEAR_SERVO_POWER, LOW);
 
-    /* Horizontal aim (rotary servo) */
-    // horizontalServo.write(horizontal);
-    // horizontalServo.writeMicroseconds(500 + ((horizontal/360) * 2000) );
-    // horizontalServo.write(0);
-    // delay(2000);
-
     delay(3000);
     horizontalServo.writeMicroseconds(newHorizontal);
-    // horizontalServo.write(180);
     Serial.println(horizontal);
-    // horizontalServo.writeMicroseconds(2500);
 
     /* Vertical aim (linear actuator) */
-    // verticalServo.writeMicroseconds(1000);
     verticalServo.writeMicroseconds(1000 + (1000 * vertical));
     // delay(2000); // Delay so linear servo can complete route
     // Cut off power to linear actuator
